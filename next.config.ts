57e59ae7375d49cb42ff49f361653240537855d9
정승국 <fightj@naver.com>
2026-01-23 15:11:40 +0900

    fix: 배포 에러 / trailingSlash: true 설정

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
-
+  trailingSlash: true,
+  
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
